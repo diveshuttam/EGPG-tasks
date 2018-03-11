4dbb7377e6f09305de63de02192c905edb78b44a
Divesh Uttamchandani <diveshuttamchandani@gmail.com>
2018-03-12 03:40:32 +0530

    added info box with main application

--- gui/file_properties.py
+++ gui/file_properties.py
@@ -2,12 +2,14 @@
 
 import pygtk
 import gtk
+import os
 # import sys
 # import argparse
 
 pygtk.require('2.0')
 
-DEBUG = True # later set this using command line arguments
+DEBUG = True  # later set this using command line arguments
+
 
 class FileSelector:
     def file_ok_handler(self, widget):
@@ -15,20 +17,33 @@ class FileSelector:
         if(DEBUG):
             print(self.selected_file)
 
-    def destroy(self, widget):
+    def debug(self, widget):
         if(DEBUG):
             print("Selected file is %s" % self.selected_file)
+
+    def destroy(self, widget):
         gtk.main_quit()
 
-    def __init__(self):
+    def destroy_connect(self, function):
+        self.file_widget.connect("destroy", function)
+
+    def __init__(self, parent=None):
         # this variable contains the selected file initilaizing to None
         self.selected_file = None
 
         # creating a file selction widget.
         self.file_widget = gtk.FileSelection("Select a file")
 
-        # connecting widgets destroy's to destroy
-        self.file_widget.connect("destroy", self.destroy)
+        # connecting debug to destroy
+        self.file_widget.connect("destroy", self.debug)
+
+        # setting parent if any
+        if (parent is not None):
+            self.file_widget.set_transient_for(parent)
+        else:
+            # connecting self.destroy to widget's destroy event which quits
+            # program
+            self.file_widget.connect("destroy", self.destroy)
 
         # ok_button
         # connect the file_ok_handler function to ok_button click
@@ -44,12 +59,122 @@ class FileSelector:
                                                       gtk.Widget.destroy,
                                                       self.file_widget)
         # show the file selector
-        self.file_widget.show()
+        self.file_widget.show_all()
+
+
+# info box for file properties
+class FilePropViewer:
+    def ok_button_handler(self, widget):
+        if(DEBUG):
+            print("Exiting message dialog")
+
+    def debug(self, widget):
+        if(DEBUG):
+            print(self.data)
+
+    def destroy(self, widget):
+        gtk.main_quit()
+
+    def __init__(self, filepath, parent=None):
+        self.data = []
+        self.filepath = filepath
+
+        self.message = gtk.MessageDialog(parent=parent, flags=0,
+                                         type=gtk.MESSAGE_INFO,
+                                         buttons=gtk.BUTTONS_OK,
+                                         message_format=None)
+
+        self.data.append("you selected %s" % filepath)
+        self.message.set_markup("\n".join(self.data))
+
+        # connecting debug to destroy
+        self.message.connect("destroy", self.debug)
+
+        if parent is None:
+            # connecting self.destroy to widget's destroy event which quits
+            # program
+            self.message.connect("destroy", self.destroy)
+
+        # ok_button
+        self.ok_button = self.message.action_area.get_children()[0]
+        # connect the file_ok_handler function to ok_button click
+        self.ok_button.connect("clicked", self.ok_button_handler)
+        # connect destroy to ok_button next i.e exit after selecting file
+        self.ok_button.connect_object("clicked", gtk.Widget.destroy,
+                                      self.message)
+        # show the info
+        self.message.show_all()
+
+
+class Application:
+    def debug(self, widget):
+        print("exiting application")
+
+    def destroy(self, widget):
+        gtk.main_quit()
+
+    def view_prop_handler(self, widget):
+        selected_file = self.file_selector.selected_file
+        if(DEBUG):
+            print("in prop handler, selected file:" + selected_file)
+        if(selected_file is not None):
+            # create a file prop dialog with selected file
+            FilePropViewer(selected_file, self.window)
+
+    def file_button_handler(self, widget):
+        self.file_selector = FileSelector(self.window)
+        self.file_selector.destroy_connect(self.view_prop_handler)
+
+    def __init__(self, parent=None):
+        # creating a file selction widget.
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+
+        # add border
+        self.window.set_border_width(50)
+
+        # connecting debug to destroy
+        self.window.connect("destroy", self.debug)
+
+        # setting parent if any
+        if (parent is not None):
+            self.window.set_transient_for(parent)
+        else:
+            # connecting self.destroy to widget's destroy event which quits
+            # program
+            self.window.connect("destroy", self.destroy)
+
+        # ok_button
+        self.file_button = gtk.Button("Select a file")
+        # quit_button
+        self.quit_button = gtk.Button("Quit")
+
+        # connect the file_ok_handler function to ok_button click
+        self.file_button.connect("clicked", self.file_button_handler)
+
+        # connect the quit button to destroy event
+        self.quit_button.connect_object("clicked", gtk.Window.destroy,
+                                        self.window)
+
+        # create a horizontal box
+        self.box1 = gtk.HBox(homogeneous=False, spacing=10)
+
+        # add buttons to boxes
+        # experiment with these operations
+        self.box1.pack_start(child=self.file_button, expand=True, fill=True,
+                             padding=10)
+        self.box1.pack_start(child=self.quit_button, expand=True, fill=True,
+                             padding=10)
+
+        # add box to window
+        self.window.add(self.box1)
+
+        # show the file selector
+        self.window.show_all()
+
 
 def main():
-    filesel = FileSelector()
+    Application()
     gtk.main()
-    print(filesel.selected_file)
 
 
 if __name__ == "__main__":
--- gui/readme.md
+++ gui/readme.md
@@ -1,9 +1,39 @@
 ### main components to be implemented:
 - [x] file selection box
-- [ ] info box (properties of file)
+- [x] info box (properties of file)
 - [ ] radio buttons for selecting the output level (verbosity)
 - [ ] check boxes for selecting fields to display
 - [ ] message / dialog box (yes/no) say for quit
 
+Chose this because:
 
-trials folder contains the scripts that I created for learning PyGTK
+implementing above components gives a decent overview of implementing
+gui's
+
+also implementing this has some common denominator with implementing gui for egpg,
+in the above example, I can just call a subshell (using subprocess/popen) and
+pass the filename to egpg to gain some of the initial features
+
+implemented in gtk as gnome applications use gtk, also nautilus and other file
+managers uses gtk so thought that it can be helpful in future
+
+.trials/ folder contains the scripts that I created for learning PyGTK
+
+### references
+http://www.pygtk.org/pygtk2tutorial/sec-FileSelections.html
+http://mailman.daa.com.au/cgi-bin/pipermail/pygtk/2007-June/013983.html
+https://stackoverflow.com/questions/8280851/pygtk-access-buttons-in-gtk-messagedialog
+
+### todo
+divide into 3 different modules, each of which can be used as a libraray as
+well as direct command line script. Using python's `__name__ == "__main__"`
+
+
+## requirements
+python2
+pygtk
+
+
+## Issues
+This is amongst my first gtk applications, so I may have implemented things in
+a lengthier/ non standard way. 
